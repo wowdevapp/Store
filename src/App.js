c1f4cbe12e1153905797558160982ff0603249f3
abdelhaq fouad <m.eng.abdelhaq@gmail.com>
2020-05-28 18:14:43 +0200

    first commit of my store

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>helloworl</h1>
+    </div>
+  );
+}
+
+export default App;
