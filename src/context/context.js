8d3ab8a5d5c56668cf82a08400bc9f00412668be
abdelhaq fouad <m.eng.abdelhaq@gmail.com>
2020-05-29 01:00:51 +0200

    sidebar more about links and styled component

--- src/context/context.js
+++ src/context/context.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import {linkData} from "./linkData";
 const ProductContext = React.createContext();
 //Provider
 //Consumer
@@ -7,7 +7,8 @@ class ProductProvider extends Component {
   state = {
     sidebarOpen: false,
     cartOpen: false,
-    cartItems: 0
+    cartItems: 0,
+    links:linkData,
   };
   // handle sidebar
   handleSidebar = () => {
